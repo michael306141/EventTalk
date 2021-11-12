78f29293cec78b75ede0932d2651f3a4838d431b
Michael Garcia <michael306.g@gmail.com>
2021-11-12 12:55:18 -0600

    Initial Commit

---
+++ Event Talk/Event_TalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_TalkApp.swift
+//  Event Talk
+//
+//  Created by Michael Garcia on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Event_TalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
