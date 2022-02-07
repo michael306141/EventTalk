d74a266618d831efb6c1e878f64bb064546664e2
Michael Garcia <michael306.g@gmail.com>
2022-02-06 21:09:35 -0600

    Main Page UI finished

--- Event Talk/ContentView.swift
+++ Event Talk/ContentView.swift
@@ -9,8 +9,213 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            
+            //Featured Event Button
+            Button {
+                
+            } label: {
+                ZStack {
+                    
+                    
+                    Image("concert2")
+                        .resizable()
+                        .aspectRatio(contentMode:.fill)
+                        .frame(width: 350, height: 400)
+                        .cornerRadius(25)
+                    
+                    Text("Create an Event")
+                        .foregroundColor(.black)
+                        .bold()
+                        .padding(.bottom, 300)
+                        .font(.largeTitle)
+                    
+                }
+                
+            }
+            
+            
+            // Nearby Event Label
+            HStack() {
+                Text("Nearby")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                Spacer()
+            }
+            // Nearby Event List
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    Button {
+                    
+                    } label: {
+                    ZStack {
+                        
+                        
+                        Image("location")
+                            .resizable()
+                            .aspectRatio(contentMode:.fill)
+                            .frame(width: 250, height: 100)
+                            .cornerRadius(25)
+                        
+                        Text("Create an Event")
+                            .foregroundColor(.black)
+                            .bold()
+                            .padding(.bottom, 60)
+                            .font(.body)
+                        
+                    }
+                    .padding()
+                    
+                }
+                        
+                    
+                    Button {
+                    
+                    } label: {
+                    ZStack {
+                        
+                        
+                        Image("concert2")
+                            .resizable()
+                            .aspectRatio(contentMode:.fill)
+                            .frame(width: 250, height: 100)
+                            .cornerRadius(25)
+                        
+                        Text("Create an Event")
+                            .foregroundColor(.black)
+                            .bold()
+                            .padding(.bottom, 60)
+                            .font(.body)
+                        
+                    }
+                    .padding()
+                    
+                }
+                        
+                    
+                    Button {
+                        
+                    } label: {
+                        ZStack {
+                            
+                            
+                            Image("location")
+                                .resizable()
+                                .aspectRatio(contentMode:.fill)
+                                .frame(width: 250, height: 100)
+                                .cornerRadius(25)
+                            
+                            Text("Create an Event")
+                                .foregroundColor(.black)
+                                .bold()
+                                .padding(.bottom, 60)
+                                .font(.body)
+                            
+                        }
+                        .padding()
+                        
+                    }
+                        
+                }
+            }
+            
+            
+            //Recent Event Label
+            HStack() {
+                Text("Recent")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                Spacer()
+            }
+   
+            //Recent Event List
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    Button {
+                    
+                    } label: {
+                    ZStack {
+                        
+                        Rectangle()
+                            .cornerRadius(25)
+                            .foregroundColor(.brown)
+                            .frame(width: 200, height: 200)
+                        Image("location")
+                            .resizable()
+                            .aspectRatio(contentMode:.fill)
+                            .frame(width: 200, height: 200)
+                            .cornerRadius(25)
+                        
+                        Text("Create an Event")
+                            .foregroundColor(.black)
+                            .bold()
+                            .padding(.bottom, 150)
+                            .font(.body)
+                        
+                    }
+                    
+                }
+                        .padding()
+                    Spacer()
+                    Button {
+                    
+                    } label: {
+                    ZStack {
+                        
+                        Rectangle()
+                            .cornerRadius(25)
+                            .foregroundColor(.brown)
+                            .frame(width: 200, height: 200)
+                        Image("concert2")
+                            .resizable()
+                            .aspectRatio(contentMode:.fill)
+                            .frame(width: 200, height: 200)
+                            .cornerRadius(25)
+                        
+                        Text("Create an Event")
+                            .foregroundColor(.black)
+                            .bold()
+                            .padding(.bottom, 150)
+                            .font(.body)
+                        
+                    }
+                    
+                }
+                        .padding()
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        ZStack {
+                            
+                            Rectangle()
+                                .cornerRadius(25)
+                                .foregroundColor(.brown)
+                                .frame(width: 200, height: 200)
+                            Image("location")
+                                .resizable()
+                                .aspectRatio(contentMode:.fill)
+                                .frame(width: 200, height: 200)
+                                .cornerRadius(25)
+                            
+                            Text("Create an Event")
+                                .foregroundColor(.black)
+                                .bold()
+                                .padding(.bottom, 150)
+                                .font(.body)
+                            
+                        }
+                        
+                    }
+                        .padding()
+                }
+            }
+
+            
+            
+        }
     }
 }
 
@@ -19,3 +224,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
